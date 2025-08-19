91689551808aead2ec512c276debab371cb4f361
sanjeevak2012 <your-email@example.com>
2025-08-19 17:54:25 +0530

    update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def hello():
+    return "Hello from My NEW WebApp with Kubernetes and Monitoring!"
